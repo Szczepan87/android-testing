be7b876b5f422ff0aca1ddadf903736c9615981c
Szczepan <l.szczepan@wp.pl>
2020-08-21 11:23:17 +0200

    set up @Before, first part of codelab ended

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -6,9 +6,9 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.android.architecture.blueprints.todoapp.Event
 import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
-import org.hamcrest.CoreMatchers.not
-import org.hamcrest.CoreMatchers.nullValue
+import org.hamcrest.CoreMatchers.*
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -19,11 +19,16 @@ class TasksViewModelTest {
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
 
-    val observer = Observer<Event<Unit>> {}
+    private val observer = Observer<Event<Unit>> {}
+    private lateinit var tasksViewModel: TasksViewModel
+
+    @Before
+    fun setUp() {
+        tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+    }
 
     @Test
     fun addNewTask_setsNewTaskEvent() {
-        val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
 
         tasksViewModel.newTaskEvent.observeForever(observer)
 
@@ -32,5 +37,15 @@ class TasksViewModelTest {
         val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
         assertThat(value.getContentIfNotHandled(), (not(nullValue())))
 
+    }
+
+    @Test
+    fun setFilterAllTasks_tasksAddViewVisible() {
+
+        tasksViewModel.setFiltering(TasksFilterType.ALL_TASKS)
+
+        val value = tasksViewModel.tasksAddViewVisible.getOrAwaitValue()
+
+        assertThat(value, `is`(true))
     }
 }
